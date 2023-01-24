9aeea8e3d3a10ea9a78b00b85652e9db04571d83
hry2566 <86605611+hry2566@users.noreply.github.com>
2023-01-25 05:27:30 +0900

    Update cls_user_form.cs

--- cls_user_form.cs
+++ cls_user_form.cs
@@ -133,10 +133,10 @@ namespace SWD4CS
             selectFlag = flag;
             selectBox!.SetSelectBoxPos(selectFlag);
             ShowProperty(flag);
-            if (flag)
-            {
-                mainForm!.ctrlTree!.SelectedNode = mainForm.ctrlTree.TopNode;
-            }
+            // if (flag)
+            // {
+            //     mainForm!.ctrlTree!.SelectedNode = mainForm.ctrlTree.TopNode;
+            // }
             // mainForm!.eventView!.ShowEventList(flag, this);
         }
         internal void SelectAllClear()
@@ -147,6 +147,102 @@ namespace SWD4CS
             {
                 CtrlItems[i].Selected = false;
             }
+        }
+        internal void CtrlAllClear()
+        {
+            // CountInit();
+
+            for (int i = 0; i < CtrlItems!.Count; i++)
+            {
+                CtrlItems[i].Selected = true;
+            }
+            RemoveSelectedItem();
+        }
+        internal void RemoveSelectedItem()
+        {
+            for (int i = 0; i < CtrlItems!.Count; i++)
+            {
+                if (CtrlItems[i].Selected)
+                {
+                    if (CtrlItems[i].ctrl is TabPage)
+                    {
+                        TabControl? tabctrl = CtrlItems[i].ctrl!.Parent as TabControl;
+
+                        if (tabctrl!.TabPages.Count > 1)
+                        {
+                            Delete(CtrlItems[i]);
+                            i--;
+                        }
+                    }
+                    else
+                    {
+                        Delete(CtrlItems[i]);
+                        i--;
+                    }
+                }
+            }
+        }
+        private void Delete(cls_controls ctrl)
+        {
+            for (int i = 0; i < CtrlItems.Count; i++)
+            {
+                if (ctrl.ctrl == CtrlItems[i].ctrl!.Parent)
+                {
+                    Delete(CtrlItems[i]);
+                    i--;
+                }
+
+                if (ctrl.ctrl is SplitContainer)
+                {
+                    SplitContainer? splcontainer = ctrl.ctrl as SplitContainer;
+
+                    for (int j = 0; j < CtrlItems.Count; j++)
+                    {
+                        if (splcontainer!.Panel1 == CtrlItems[j].ctrl!.Parent || splcontainer!.Panel2 == CtrlItems[j].ctrl!.Parent)
+                        {
+                            Delete(CtrlItems[j]);
+                            i--;
+                        }
+                    }
+                }
+            }
+            ctrl.Delete();
+            CtrlItems.Remove(ctrl);
+        }
+        internal void Add_Controls(List<CONTROL_INFO> ctrlInfo)
+        {
+            // コントロール全削除
+            CtrlAllClear();
+
+            // Control作成
+            for (int i = 0; i < ctrlInfo.Count; i++)
+            {
+                _ = new cls_controls(this, this, ctrlInfo[i]);
+            }
+            this.Location = new Point(16, 16);
+
+            // その他設定
+
+            for (int i = 0; i < ctrlInfo.Count; i++)
+            {
+                for (int j = 0; j < CtrlItems.Count; j++)
+                {
+                    if (CtrlItems[j].ctrl != null)
+                    {
+                        if (CtrlItems[j].ctrl!.Name == ctrlInfo[i].ctrlName || ctrlInfo[i].ctrlName == "this")
+                        {
+                            CtrlItems[j].SetControls(ctrlInfo[i]);
+                        }
+                    }
+                }
+            }
+            // selectbox設定
+            for (int j = 0; j < CtrlItems.Count; j++)
+            {
+                CtrlItems[j].InitSelectBox();
+            }
+
+            SelectAllClear();
         }
 
 
